ccdd272d441ca3096b87da32609400ac100eaa94
Andre Gil <andre.gil@onespan.com>
2023-01-25 17:48:53 -0500

    Initial commit

---
+++ src/test/java/org/agil/copilot/CopilotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.agil.copilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopilotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
